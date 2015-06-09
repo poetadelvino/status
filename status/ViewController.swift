9a34a56025b4b13c97e03c2404f1f45451ca6747
poetadelvino <almanegraecm@gmail.com>
2015-06-08 19:28:55 -0700

    Added tableView, with Data source

--- status/ViewController.swift
+++ status/ViewController.swift
@@ -29,18 +29,30 @@ the famous one is the user model, or user class
 
 we could have given this a better name, like StatusViewController
 */
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        
+        //  of update itemsTODO: Return count
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        // TODO: make this cell reuseable
+        
+        var cell = UITableViewCell()
+        return cell
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
